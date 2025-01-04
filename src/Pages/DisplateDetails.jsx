96a2d8080c9d3a247b8fdbd3c54b6c53ea1f284a
Momen <touristmomen@gmail.com>
2025-01-04 21:49:38 +0600

    details more design and drawer added

--- src/Pages/DisplateDetails.jsx
+++ src/Pages/DisplateDetails.jsx
@@ -8,6 +8,11 @@ const DisplateDetails = () => {
     "https://static.displate.com/230x320/displate/2024-10-29/d0d9014a05de31c07beff9fe2f5c2962_a541c51b3c30e670c7d69684ceaee944.avif"
   );
   const [frameColor, setFrameColor] = useState("black");
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   const images = [
     "/images/main.jpg",
@@ -31,7 +36,7 @@ const DisplateDetails = () => {
         <span className="text-white font-semibold">Crossing the Bridge</span>
       </h1>
       <div className="flex flex-wrap lg:flex-nowrap gap-8">
-        <div className="relative bg-black w-full lg:w-[70%]">
+        <div className="relative rounded-lg bg-black w-full lg:w-[70%]">
           <div className="flex items-center h-screen">
             <img
               src={selectedImage}
@@ -60,23 +65,64 @@ const DisplateDetails = () => {
           </div>
         </div>
         <div className="flex-grow">
-          <h1 className="text-3xl font-bold mb-4">Crossing the Bridge</h1>
-          <p className="text-gray-300 mb-6">
-            Jinx, the chaotic heart of Arcane, walks a
-          </p>
-          <p className="text-lg font-semibold mb-6">Read more</p>
-          <div className="flex items-center mb-6">
-            <p className="text-xl font-semibold mr-4">4.7/5</p>
-            <div className="flex text-yellow-400">
+          <h1 className="text-xl font-semibold">Crossing the Bridge</h1>
+          <div>
+            <p className="w-[350px]">
+              {isExpanded
+                ? "In this poignant artwork from Arcane Season 2, the bond between Jinx and Vi is palpable. The contrast of their vibrant hair and intense gazes speaks volumes about their complicated relationship, leaving the viewer to ponder the depths of their connection. Riot Games' distinctive visual style is beautifully captured in the painterly details and dramatic lighting."
+                : "In this poignant artwork from Arcane Season...  "}
+            </p>
+            <button className="text-[#1185ed] font-bold" onClick={toggleText}>
+              {isExpanded ? "Read less" : "Read more"}
+            </button>
+          </div>
+          <div className="flex items-center mb-2">
+            <p className="mr-2">4.7/5</p>
+            <div className="flex text-lg text-green-500">
               {[...Array(5)].map((_, index) => (
                 <span key={index}>&#9733;</span>
               ))}
             </div>
-            <p className="ml-4 text-sm text-gray-400">(15,439 reviews)</p>
+            <p className="ml-2 text-sm text-[#1185ed] font-bold">
+              (15,439 reviews)
+            </p>
           </div>
-          <div className="mb-2">
-            <h2 className="text-base font-semibold mb-2">
+          <div className="mb-2 group">
+            <h2 className="text-base font-semibold mb-2 flex items-center gap-2">
               Select Product Type
+              <span>
+                <div className="drawer drawer-end z-50">
+                  <input
+                    id="my-drawer-4"
+                    type="checkbox"
+                    className="drawer-toggle"
+                  />
+                  <div className="drawer-content flex">
+                    <label
+                      htmlFor="my-drawer-4"
+                      className="drawer-button text-[#1185ed] font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                    >
+                      Learn more
+                    </label>
+                  </div>
+                  <div className="drawer-side">
+                    <label
+                      htmlFor="my-drawer-4"
+                      aria-label="close sidebar"
+                      className="drawer-overlay"
+                    ></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                      {/* Sidebar content here */}
+                      <li>
+                        <a>Sidebar Item 1</a>
+                      </li>
+                      <li>
+                        <a>Sidebar Item 2</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </span>
             </h2>
             <div className="w-2/3 bg-[#eceff4] py-1 rounded-full mb-6">
               <button
